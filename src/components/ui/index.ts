19c1b1b15c3c310bc3a93629c28ee6d546e4b722
Tyler Straffon <tylerstraffon@gmail.com>
2026-03-25 21:03:31 -0600

    F R E S H  S T A R T.

--- src/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from './Button'
-export type { ButtonProps } from './Button'
-
-export { Card, CardHeader, CardBody, CardFooter } from './Card'
-export type { CardProps } from './Card'
-
-export { Input } from './Input'
-export type { InputProps } from './Input'
